38987688b9a19d7e18bf135bfd94ece59e01db6a
Kathy White <kathywh@yandex.com>
2018-05-14 21:20:47 +0800

    Add instruction memory & data memory

---
+++ rtl/DataMemory.v
@@ -0,0 +1,38 @@
+/******************************************************************************/
+/*  Unit Name:  DataMemory                                                    */
+/*  Created by: Kathy                                                         */
+/*  Created on: 05/14/2018                                                    */
+/*  Edited by:  Kathy                                                         */
+/*  Edited on:  05/14/2018                                                    */
+/*                                                                            */
+/*  Description:                                                              */
+/*      Data memory wrapper for block RAM in different FPGAs.                 */
+/*                                                                            */
+/*  Revisions:                                                                */
+/*      05/14/2018  Kathy       Unit created.                                 */
+/******************************************************************************/
+
+module DataMemory
+(
+  input Clock,
+  input [29:0] Addr,
+  input En_W, En_R,
+  input [31:0] Data_W,
+  output [31:0] Data_R
+);
+
+`ifdef ALT_EP4CE
+  Alt_EP4CE_DataMem_4KB D_Mem
+  (
+    .address(Addr[7:0]),
+    .clock(Clock),
+    .data(Data_W),
+    .rden(En_R),
+    .wren(En_W),
+    .q(Data_R)
+  );
+`elsif XIL_XC6SLX
+  /* TO DO */
+`endif
+  
+endmodule
---
+++ rtl/InstructionMemory.v
@@ -0,0 +1,40 @@
+/******************************************************************************/
+/*  Unit Name:  InstructionMemory                                             */
+/*  Created by: Kathy                                                         */
+/*  Created on: 05/14/2018                                                    */
+/*  Edited by:  Kathy                                                         */
+/*  Edited on:  05/14/2018                                                    */
+/*                                                                            */
+/*  Description:                                                              */
+/*      Instruction memory wrapper for block RAM in different FPGAs.          */
+/*                                                                            */
+/*  Revisions:                                                                */
+/*      05/14/2018  Kathy       Unit created.                                 */
+/******************************************************************************/
+
+module InstructionMemory
+(
+  input Clock,
+  input En_I, En_D,
+  input [28:0] Addr_I, Addr_D,
+  output [31:0] Data_I, Data_D
+);
+
+`ifdef ALT_EP4CE
+  // Port a - I
+  // Port b - D
+  Alt_EP4CE_InstrMem_1KB I_Mem
+  (
+    .address_a(Addr_I[7:0]),
+    .address_b(Addr_D[7:0]),
+    .clock(Clock),
+    .rden_a(En_I),
+    .rden_b(En_D),
+    .q_a(Data_I),
+    .q_b(Data_D)
+  );
+`elsif XIL_XC6SLX
+  /* TO DO */
+`endif
+  
+endmodule
