13e9d577364e6182b7bdb610c28cfea59145e264
Kathy White <kathywh@yandex.com>
2018-05-15 22:13:26 +0800

    Rewrite module declaration in a different style

--- rtl/RegisterFile.v
+++ rtl/RegisterFile.v
@@ -3,7 +3,7 @@
 /*  Created by: Kathy                                                         */
 /*  Created on: 04/06/2018                                                    */
 /*  Edited by:  Kathy                                                         */
-/*  Edited on:  04/12/2018                                                    */
+/*  Edited on:  05/15/2018                                                    */
 /*                                                                            */
 /*  Description:                                                              */
 /*      Register file with zero register.                                     */
@@ -15,23 +15,24 @@
 /*      04/07/2018  Kathy       Add addr reg reset.                           */
 /*      04/12/2018  Kathy       1) Remove reset signal and functionality.     */
 /*                              2) Remove read addr reg, register read data.  */
+/*      05/15/2018  Kathy       Rewrite module in a different style.          */
 /******************************************************************************/
 
-`define  REGFL_IDX_ZR  {W_ADDR{1'b1}}     // index of zero reg
-`define  REGFL_ZERO    {W_DATA{1'b0}}     // zero value
-
 module RegisterFile
-#(
-  parameter W_DATA = 32,      // data width
-  parameter W_ADDR = 5        // addr width
-)
 (
-  input Clock,
-  input EnX, EnY, EnW,
-  input [W_ADDR-1:0] AddrX, AddrY, AddrW,
-  output reg [W_DATA-1:0] DataX, DataY,
-  input [W_DATA-1:0] DataW
+  Clock, EnX, EnY, EnW,
+  AddrX, AddrY, AddrW,
+  DataX, DataY, DataW
 );
+
+  localparam W_DATA = 32;      // data width
+  localparam W_ADDR = 5;       // addr width
+
+  input Clock;
+  input EnX, EnY, EnW;
+  input [W_ADDR-1:0] AddrX, AddrY, AddrW;
+  output reg [W_DATA-1:0] DataX, DataY;
+  input [W_DATA-1:0] DataW;
 
   localparam C_REG = 2 ** W_ADDR - 1;       // reg count, -1 because last reg reads zero.
 
@@ -40,7 +41,7 @@ module RegisterFile
   // Write reg
   always @(posedge Clock)
     begin      
-      if(EnW && (AddrW != `REGFL_IDX_ZR))
+      if(EnW && (AddrW != `IDX_ZR))
         begin
           RegArray[AddrW] <= DataW;
         end
@@ -52,15 +53,15 @@ module RegisterFile
       // Port X
       if(EnX)
         begin
-          if(AddrX == `REGFL_IDX_ZR)        // zero reg
+          if(AddrX == `IDX_ZR)            // zero reg
             begin
-              DataX <= `REGFL_ZERO;
+              DataX <= 0;
             end
-          else if(AddrX == AddrW)           // write-through
+          else if(AddrX == AddrW)         // write-through
             begin
               DataX <= DataW;
             end
-          else                              // read array reg
+          else                            // read array reg
             begin
               DataX <= RegArray[AddrX];
             end
@@ -69,15 +70,15 @@ module RegisterFile
       // Port Y
       if(EnY)
         begin
-          if(AddrY == `REGFL_IDX_ZR)        // zero reg
+          if(AddrY == `IDX_ZR)            // zero reg
             begin
-              DataY <= `REGFL_ZERO;
+              DataY <= 0;
             end
-          else if(AddrY == AddrW)           // write-through
+          else if(AddrY == AddrW)         // write-through
             begin
               DataY <= DataW;
             end
-          else                              // read array reg
+          else                            // read array reg
             begin
               DataY <= RegArray[AddrY];
             end
