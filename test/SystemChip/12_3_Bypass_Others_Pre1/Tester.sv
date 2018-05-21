52ddf92fce0da75eab02f071afb02516e24cf693
Kathy White <kathywh@yandex.com>
2018-05-21 17:41:38 +0800

    Add bypass & pipeline stall test for Kabeta

---
+++ test/SystemChip/12_3_Bypass_Others_Pre1/Tester.sv
@@ -0,0 +1,95 @@
+/******************************************************************************/
+/*  Unit Name:  Tester                                                        */
+/*  Created by: Kathy                                                         */
+/*  Created on: 05/18/2018                                                    */
+/*  Edited by:  Kathy                                                         */
+/*  Edited on:  05/18/2018                                                    */
+/*                                                                            */
+/*  Description:                                                              */
+/*      Tester for Kabeta.                                                    */
+/*                                                                            */
+/*  Revisions:                                                                */
+/*      05/18/2018  Kathy       Unit created.                                 */
+/******************************************************************************/
+
+program Tester
+#(
+  parameter TCLK
+)
+( 
+  input Clock,
+  output logic Reset
+);
+
+  logic [31:0] TestData[] =
+  '{
+      32'h0000_5A01,
+      32'h0000_5A02,
+      32'h0000_004C,
+      32'h0000_0054
+  };
+
+  initial
+    begin
+`ifdef FSDB_DUMP
+      // set fsdb parameters
+      $fsdbDumpfile("SystemChip.fsdb");
+      $fsdbDumpvars;
+`endif
+      // Drive reset
+      Reset = 1'b0;
+      #(TCLK/4) Reset = 1'b1;
+    end
+
+  initial
+    begin
+      logic [4:0] index;
+      logic [31:0] value;
+      logic wen;
+
+      bit Pass = 1;
+
+      // Wait for reset (2)
+      repeat(2) @(posedge Clock);
+
+      // BR @ reset vector (1+2), switch mode (2+2)
+      // NOTE: +2 is for branch delay slots
+      repeat(7) @(posedge Clock);
+
+      // Instructions before loop (4)
+      repeat(4) @(posedge Clock);
+
+      // WB-Stage delay (4)
+      repeat(4) @(posedge Clock);
+
+      // Bypass
+      for(int i=0; i<$size(TestData); i++)
+        begin
+          // Instructions before bypass (1)
+          @(posedge Clock);
+
+          index = Testbench.DesignTop.KabCore.RF.AddrW;
+          value = Testbench.DesignTop.KabCore.RF.DataW;
+          wen = Testbench.DesignTop.KabCore.RF.EnW;
+
+          if((wen !== 1'b1) || (index !== 5'd2))
+            begin
+              Pass = 0;
+              $display(">> ERROR (@%0t): MOV[%0d]: Incorrect reg index or wen, maybe wrong cycle.", $time, i);
+            end
+          if(value !== TestData[i])
+            begin
+              Pass = 0;
+              $display(">> ERROR (@%0t) MOV[%0d]: Incorrect bypass: value=%x", $time, i, value);
+            end
+          @(posedge Clock);
+        end
+
+      // Print status message
+      if(Pass)
+        $display(">>>> Kabeta: Pass.");
+      else
+        $display(">>>> Kabeta: FAIL.");
+    end
+
+endprogram
