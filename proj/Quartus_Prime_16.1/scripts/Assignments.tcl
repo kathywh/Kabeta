1f74b4c73e6a9e1ba93adbcee25a7d40a01f7496
Kathy White <kathywh@yandex.com>
2018-05-28 16:16:31 +0800

    Update project settings   Set unused pins as input tri-stated

--- proj/Quartus_Prime_16.1/scripts/Assignments.tcl
+++ proj/Quartus_Prime_16.1/scripts/Assignments.tcl
@@ -6,6 +6,7 @@
 set_global_assignment -name FAMILY "Cyclone IV E"
 set_global_assignment -name DEVICE EP4CE6F17C8
 set_global_assignment -name STRATIX_DEVICE_IO_STANDARD "3.3-V LVCMOS"
+set_global_assignment -name RESERVE_ALL_UNUSED_PINS_WEAK_PULLUP "AS INPUT TRI-STATED"
 set_global_assignment -name CYCLONEII_RESERVE_NCEO_AFTER_CONFIGURATION "USE AS REGULAR IO"
 
 # Configuration Settings
