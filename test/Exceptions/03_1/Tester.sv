dbe2f4508c63f4e907ad9fd9b5284bb3da32ffac
Kathy White <kathywh@yandex.com>
2018-06-06 11:50:44 +0800

    Add exceptions test 03_1

---
+++ test/Exceptions/03_1/Tester.sv
@@ -0,0 +1,34 @@
+program Tester;
+
+  wire Sys_Clock = Testbench.DesignTop.Sys_Clock;
+  wire Sys_Reset  = Testbench.DesignTop.Sys_Reset;
+
+  initial
+    begin
+`ifdef FSDB_DUMP
+      // set fsdb parameters
+      $fsdbDumpfile("SystemChip.fsdb");
+      $fsdbDumpvars;
+`endif
+    end
+
+  initial
+    begin
+      // Wait for reset
+      wait(Sys_Reset == '1);
+
+      // Wait for branch
+      repeat(11) @(posedge Sys_Clock);
+
+      // Generate interrupt
+      force Testbench.DesignTop.KAB_CORE.KIU.KIU_IntReq = '1;
+      force Testbench.DesignTop.KAB_CORE.KIU.KIU_IntId = '0;
+      @(posedge Sys_Clock);
+      force Testbench.DesignTop.KAB_CORE.KIU.KIU_IntReq = '0;
+      release Testbench.DesignTop.KAB_CORE.KIU.KIU_IntId;
+
+      // Wait for running
+      repeat(40) @(posedge Sys_Clock);
+    end
+
+endprogram
--- test/Exceptions/readme.txt
+++ test/Exceptions/readme.txt
@@ -1,3 +1,7 @@
 01:  RR-Stage Fault in branch slots
 02_1:  IF-Stage Fault in branch slots (User Mode)
 02_2:  IF-Stage Fault in branch slots (Supervisor Mode)
+03_1:  Interrupt when branch at IF-Stage
+03_2:  Interrupt when branch at RR-Stage
+03_3:  Interrupt when branch at EX-Stage
+03_4:  Interrupt when branch at MA-Stage
